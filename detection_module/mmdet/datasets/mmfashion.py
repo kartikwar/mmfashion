ac53e2a1e02aab96bc4fbfa139c8c171bd9d295f
kartikwar <kartiksirwani@gmail.com>
2020-07-30 15:50:17 +0530

    removed mm detection submodule

--- detection_module/mmdet/datasets/mmfashion.py
+++
@@ -1,9 +0,0 @@
-from .coco import CocoDataset
-from .registry import DATASETS
-
-
-@DATASETS.register_module
-class mmfashionDataset(CocoDataset):
-    CLASSES = ('top', 'skirt', 'leggings', 'dress', 'outer', 'pants', 'bag',
-               'neckwear', 'headwear', 'eyeglass', 'belt', 'footwear', 'hair',
-               'skin', 'face')
--- detection_module/mmdet/version.py
+++
@@ -1,6 +0,0 @@
-# GENERATED VERSION FILE
-# TIME: Thu Jul 30 00:23:11 2020
-
-__version__ = '1.2.0+unknown'
-short_version = '1.2.0'
-version_info = (1, 2, 0)
